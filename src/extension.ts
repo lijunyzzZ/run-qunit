a87e01eb31c3934825cc6af423dcf1372baef53a
lijuny <1920271554@qq.com>
2024-04-13 12:02:09 +0800

    feat: 实现cmd + shift + r 直接执行action.ts中的用例方法

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const executeIcon = new vscode.ThemeIcon("debug-alt", new vscode.ThemeColor("#98cf8c"));
+
+
+console.log(executeIcon);
+
+const settins = vscode.workspace.getConfiguration("run-qunit");
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -20,7 +27,73 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+	// 在当前活动的编辑器中添加图标
+	
+	// 在命令注册时调用该函数
+	disposable = vscode.commands.registerCommand('run-qunit.runaction', () => {
+		let editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return
+		}
+		let path = editor?.document.uri.path
+		if (path.endsWith("action.ts")) {
+			let start = path.indexOf("/static-file/test/");
+			let urlPath = path.substring(start + "/static-file/test/".length, path.lastIndexOf('.'));
+			let position = editor.selection.active;
+			let line = position.line;
+			let startLine = line, endLine = line;
+			let document = editor.document;
+			let max = document.lineCount;
+			let getFuncName = (lineNumber: number) => {
+				let value = document.lineAt(lineNumber)?.text;
+				if (!value) {
+					return;
+
+				}
+				let funcIndex = value.indexOf("export function test");
+				if (funcIndex == -1) {
+					return;
+				}
+				let funcEnd = value.indexOf("(assert: Assert)");
+				if (funcEnd != -1) {
+					return value.substring(funcIndex + "export function test_".length, funcEnd );
+				}
+			}
+			let funcName;
+			while (startLine > 0 || endLine <= max) {
+				if (startLine > 0) {
+					funcName = getFuncName(startLine);
+					if (funcName) {
+						break;
+					}
+					if (startLine == endLine) {
+						endLine++;
+					}
+					startLine--;
+				}
+				if (endLine < max) {
+					let text = document.lineAt(endLine)?.text
+					if (text == '}') {
+						endLine = max;
+						continue;
+					}
+					funcName = getFuncName(endLine);
+					if (funcName) {
+						break;
+					}
+					endLine++;
+				}
+			}
+
+			let url = `http://127.0.0.1:8080/test/${urlPath}?filter=${funcName}`;
+			vscode.env.openExternal(vscode.Uri.parse(url));
+		}
+
+	});
+	context.subscriptions.push(disposable);
+
+
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
